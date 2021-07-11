fd5fe5b7815710d677e9baea42000ec3f47312b8
WhiteBlackGoose <wbg@angouri.org>
2021-07-11 16:57:04 +0300

    Benchmark

---
+++ VariadicNumber/PerfBenchmark.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+
+public class BenchVariadicNumber
+{
+    const int OperationsPerInvoke = 300;
+
+    public VariadicNumber[] numbersInt = new VariadicNumber[OperationsPerInvoke];
+    public VariadicNumber[] numbersFloat = new VariadicNumber[OperationsPerInvoke];
+    public VariadicNumber[] numbersDouble = new VariadicNumber[OperationsPerInvoke];
+    private int currId = 0;
+
+    [Benchmark(OperationsPerInvoke = OperationsPerInvoke)]
+    public void SumAllInt()
+    {
+        VariadicNumber a = 0;
+        foreach (var el in numbersInt)
+            a += (el + 1) / (a + 1);
+        numbersInt[currId] = a;
+        currId = (currId + 1) % OperationsPerInvoke;
+    }
+
+    [Benchmark(OperationsPerInvoke = OperationsPerInvoke)]
+    public void SumAllFloat()
+    {
+        VariadicNumber a = 0f;
+        foreach (var el in numbersFloat)
+            a += (el + 1) / (a + 2);
+        numbersFloat[currId] = a;
+        currId = (currId + 1) % OperationsPerInvoke;
+    }
+
+    [Benchmark(OperationsPerInvoke = OperationsPerInvoke)]
+    public void SumAllDouble()
+    {
+        VariadicNumber a = 0d;
+        foreach (var el in numbersDouble)
+            a += (el + 1) / (a + 2);
+        numbersDouble[currId] = a;
+        currId = (currId + 1) % OperationsPerInvoke;
+    }
+}
--- VariadicNumber/Program.cs
+++ VariadicNumber/Program.cs
@@ -1,4 +1,18 @@
 ﻿VariadicNumber a = 5;
-Console.WriteLine($"a = {a} ; a / 2 = {a / 2}");
+Console.WriteLine($"a = {a.ToString(true)} ; a / 2 = {a / 2}");
 VariadicNumber b = 5.0;
-Console.WriteLine($"b = {b} ; b / 2 = {b / 2}");
+Console.WriteLine($"b = {b.ToString(true)} ; b / 2 = {b / 2}");
+
+
+
+// 
+// Benchmarking results:
+// a + (b + 1) / (a + 2) costs as much as 65 ns
+// if both a and b are of the same type
+//
+
+
+// using BenchmarkDotNet.Running;
+// 
+// BenchmarkRunner.Run<BenchVariadicNumber>();
+
