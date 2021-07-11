8854508238e0c956223b28ecc3fbeeb44676aef3
WhiteBlackGoose <wbg@angouri.org>
2021-07-11 22:40:38 +0300

    Fast copy

---
+++ FastCopy/BenchCopy.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.InteropServices;
+using BenchmarkDotNet.Attributes;
+
+
+namespace FastCopyTest
+{
+    unsafe public class BenchmarkCopy
+    {
+        private int* arr1;
+        private int* arr2;
+
+        const int LENGTH = 10_000_000;
+
+        [Params(1000, 10_000, 100_000, 1_000_000)]
+        public int ToCopyLength { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            arr1 = (int*)NativeMemory.AlignedAlloc(LENGTH, 32);
+            arr2 = (int*)NativeMemory.AlignedAlloc(LENGTH, 32);
+        }
+
+        [Benchmark]
+        public void MyCopy()
+        {
+            FastCopy.CopyAligned(arr1, arr2, ToCopyLength);
+        }
+
+        [Benchmark]
+        public void MemoryCopy()
+        {
+            Buffer.MemoryCopy(arr2, arr1, LENGTH, ToCopyLength * sizeof(int));
+        }
+
+        [Benchmark]
+        public void Kernel32MoveMemory()
+        {
+            MoveMemory(arr2, arr1, (ulong)(ToCopyLength * sizeof(int)));
+        }
+
+        [Benchmark]
+        public void Straightforward()
+        {
+            FastCopy.CopyStraightForward(arr2, arr1, ToCopyLength);
+        }
+
+        [DllImport("kernel32.dll", EntryPoint = "RtlMoveMemory", SetLastError = false)]
+        static extern unsafe void MoveMemory(void* dst, void* src, ulong length);
+    }
+}
---
+++ FastCopy/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Running;
+using FastCopyTest;
+
+BenchmarkRunner.Run<BenchmarkCopy>();
+// 
+// 
+// var arrSrc = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+// var arrDst = new[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+// unsafe
+// {
+//     fixed (int* src = arrSrc)
+//     {
+//         fixed (int* dst = arrDst)
+//         {
+//             // FastCopy.Copy(dst + 2, src + 3, 9);
+//             MoveMemory(dst, src, 5);
+//         }
+//     }
+// }
+// 
+// Console.WriteLine(string.Join(", ", arrDst));
+
+
+[DllImport("kernel32.dll", EntryPoint = "RtlMoveMemory", SetLastError = false)]
+static extern unsafe void MoveMemory(void* dst, void* src, ulong length);
+
+public unsafe static class FastCopy
+{
+    public static void CopyAligned<T>(T* bDst, T* bSrc, int count) where T : unmanaged
+        => CopyPlainAligned(bDst, bSrc, sizeof(T) * count);
+
+    public static void CopyPlainAligned(void* bDst, void* bSrc, int count)
+    {
+        const int LOOP_UNROLL = 4;
+
+        var dst = (byte*)bDst;
+        var src = (byte*)bSrc;
+
+        var blockCount = count - 32 * LOOP_UNROLL;
+
+        int i;
+        for (i = 0; i < blockCount; i += 32 * LOOP_UNROLL)
+        {
+            var s = src + i;
+            var d = dst + i;
+            var vec = Avx.LoadAlignedVector256(s);
+            Avx.Store(d, vec);
+
+            s += 32; d += 32;
+            vec = Avx.LoadAlignedVector256(s);
+            Avx.Store(d, vec);
+
+            s += 32; d += 32;
+            vec = Avx.LoadAlignedVector256(s);
+            Avx.StoreAligned(d, vec);
+
+            s += 32; d += 32;
+            vec = Avx.LoadAlignedVector256(s);
+            Avx.StoreAligned(d, vec);
+        }
+
+        var blockCount2 = count - 32;
+
+        for (; i < blockCount2; i += 32)
+        {
+            var s = src + i;
+            var d = dst + i;
+            var vec = Avx.LoadAlignedVector256(s);
+            Avx.StoreAligned(d, vec);
+        }
+
+        var blockCount3 = count - 16;
+
+        for (; i < blockCount3; i += 16)
+        {
+            var vec = Avx.LoadAlignedVector128(src + i);
+            Avx.StoreAligned(dst + i, vec);
+        }
+
+        for (; i < count; i++)
+            dst[i] = src[i];
+    }
+
+    public static void CopyStraightForward<T>(T* bDst, T* bSrc, int count) where T : unmanaged
+    {
+        for (int i = 0; i < count; i++)
+            bDst[i] = bSrc[i];
+    }
+}
